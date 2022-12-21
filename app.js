52470377de2a77cc228218fe461d833d3175d3f8
Shreyash Makadia <56908143+srmakadia7110@users.noreply.github.com>
2022-12-21 15:03:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin:'http://localhost:3001',
+  origin:'*',
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }));
