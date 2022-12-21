54c970f388f2d5f645f277b054a76314ba4db096
Shreyash Makadia <56908143+srmakadia7110@users.noreply.github.com>
2022-12-21 15:47:46 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host : 'localhost',
+	host : '192.168.1.18',
 	database : 'user_management',
 	user : 'root',
 	password : ''
